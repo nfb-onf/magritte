62b3d80c07e3f6055be4b7141760a95001b501af
Marc Lavallee <marc@hacklava.net>
2013-02-11 17:56:00 -0500

    added download option for one package

--- magritte/main.py
+++ magritte/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 Magritte is a command with options to fetch python packages from a list of pip urls,
-including their dependencies, and upload them to a pypi server.
+including their dependencies, save them to a local cache or upload them to a pypi server.
 
 Usage: magritte
 (without options it displays this help)
@@ -9,6 +9,7 @@ Usage: magritte
 Options:
 -h or --help : display this help
 -r or --reset : delete all created files and directories from local cache (in ~/.magritte/)
+-d or --download <package url> : fetch one package only (will also fetch dependent packages)
 -g or --get <lists of files> : fetch packages listed from text files, in "pip urls" format
 -s or --save : build requirement file and save packages as tgz archives in ~/.magritte/dists/
 -p or --push <server>: build requirement file and push packages to a specified pypi server (defined in ~/.pypirc)
@@ -51,10 +52,12 @@ class Usage(Exception):
         self.msg = msg
 
 def main():
+    long_options = ["help", "reset", "download", "get", "save", "push", "list", "skipped", "verbose"]
+    short_options = 'hrdgsp'
+    options_dict = dict()
     try:
         try:
-            opts, args = getopt.getopt(sys.argv[1:], "hrgsp",
-                ["help", "reset", "get", "save", "push", "list", "skipped", "verbose"])
+            opts, args = getopt.getopt(sys.argv[1:], short_options, long_options)
         except getopt.error, msg:
             raise Usage(msg)
 
@@ -67,7 +70,7 @@ def main():
         print __doc__
         sys.exit(0)
 
-    verbose =  get = save = push = list_downloaded = skipped = reset = False
+    verbose = get = download = save = push = list_downloaded = skipped = reset = False
 
     for o, a in opts:
         if o in ("-h", "--help"):
@@ -77,6 +80,8 @@ def main():
         # process arguments
         if o in ("-r", "--reset"):
             reset = True
+        if o in ("-d", "--download"):
+            download = True
         if o in ("-g", "--get"):
             get = True
         if o in ("-s", "--save"):
@@ -95,7 +100,7 @@ def main():
 
     # validate combination of options
 
-    if get and (save or push):
+    if (get or download) and (save or push):
         if save:
             logger.error("Can't get and save at the same time; get, then save...")
         if push:
@@ -104,9 +109,11 @@ def main():
 
     getter = Getter()
 
-    if reset or get or list or skipped :
+    if reset or download or get or list or skipped :
         if reset:
             getter.package_cache.reset()
+        if download:
+            getter.download_package_from_url(args)
         if get:
             getter.get_packages_from_lists_of_urls(args)
         if list_downloaded:
