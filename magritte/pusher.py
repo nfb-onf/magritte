48f6b5d8ce75bdb522212f8dbbb0f8144d07ea8c
Mathieu Avoine <m.avoine@onf.ca>
2013-09-10 15:23:40 -0400

    Changed output format of the file name to make parsing (extraction of name and version) easier. warning: change not retro compatible

--- magritte/pusher.py
+++ magritte/pusher.py
@@ -16,16 +16,18 @@ class Pusher(object):
     def build_requirements(self):
         collector = Collector()
         collector.traverse(self.package_cache.all_downloaded_packages)
-        self.requirements = collector.format_requirements()
+        self.requirements = collector.get_requirements()
 
     def save_requirements_to_local_dists_dir(self):
         dists_dir = self.package_cache.dists_dir
         versions_dir = self.package_cache.versions_dir
         requirements = []
-        for requirement in self.requirements:
-            requirements.append('%s.tgz' % requirement)
-            dist_file = '%s/%s.tgz' % (dists_dir, requirement)
-            command = 'tar -zcf %s %s' % (dist_file, requirement)
+        for (req_name, req_version) in self.requirements:
+            req_file_name = '%s__%s.tgz' % (req_name, req_version)
+            req_package = "%s-%s" % (req_name, req_version)
+            requirements.append(req_file_name)
+            dist_file = '%s/%s' % (dists_dir, req_file_name)
+            command = 'tar -zcf %s %s' % (dist_file, req_package)
             args = shlex.split(command)
             logger.info("creating dist:%s", dist_file)
             p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=versions_dir)
@@ -43,9 +45,11 @@ class Pusher(object):
     def push_requirements_to_pypi_server(self, server):
         versions_dir = self.package_cache.versions_dir
         requirements = []
-        for requirement in self.requirements:
-            requirements.append(requirement.replace('-', '=='))
-            version_dir = "%s/%s" % (versions_dir, requirement)
+        for (req_name, req_version) in self.requirements:
+            req_package = "%s-%s" % (req_name, req_version)
+            req_spec = "%s==%s" % (req_name, req_version)
+            requirements.append(req_spec)
+            version_dir = "%s/%s" % (versions_dir, req_package)
             command = 'python setup.py register -r %s sdist upload -r %s' % (server, server)
             args = shlex.split(command)
             p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=version_dir)
