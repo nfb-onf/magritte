6322f0d30c941c7d61979b97bc8725744e77918f
Mathieu Avoine <m.avoine@onf.ca>
2013-11-12 14:44:37 -0500

    version 0.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if get_python_version() < MINIMAL_PYTHON_VERSION:
 
 setup(
     name = "magritte",
-    version = "0.3",
+    version = "0.4",
     packages = packages,
     author = "NFB/ONF",
     author_email = "m.lavallee@onf.ca",
